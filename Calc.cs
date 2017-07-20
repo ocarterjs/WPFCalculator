8462573a059f66d1a256a216510278b46eb7a53b
Owen Carter <Owen@kingfisher-estate.co.uk>
2017-07-20 13:24:51 +0100

    WPF C# Calculator
    A WPF C# Calculator, written in Visual Stuido either 2010/2013

---
+++ Calc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.cs
+{
+    class Calc
+    {
+        private double _sum;
+
+        public double Sum
+        {
+            get { return _sum; }
+            private set { _sum = value; }
+        }
+
+        public void Add(double num1, double num2)
+        {
+            _sum = num1 + num2;
+        }
+        public void Minus(double num1, double num2)
+        {
+            _sum = num1 - num2;
+        }
+        public void Multiply(double num1, double num2)
+        {
+            _sum = num1 * num2;
+        }
+        public void Divide(double num1, double num2)
+        {
+            _sum = num1 / num2;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator.cs
+{
+    public partial class MainWindow : Window
+    {
+        private double num1;
+        private double num2;
+
+        Calc calculator = new Calc(); // instance of calculator
+
+        private void Parse() // parse method to make life easy.
+        {
+            try
+            {
+                num1 = double.Parse(TextBox1.Text);
+                num2 = double.Parse(TextBox2.Text);
+            }
+            catch (FormatException) // catches the format exception to exclude anything but a numerical input.
+            {
+                MessageBox.Show("Please enter a valid numerical input.");
+            }
+        }
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            Parse();
+            calculator.Add(num1, num2);
+            label1.Content = calculator.Sum;
+        }
+        // Parses the data, calls the calculator method add taking in both parameters and returns value to label1.content.
+
+        private void Minus_Click(object sender, RoutedEventArgs e)
+        {
+            Parse();
+            calculator.Minus(num1, num2);
+            label1.Content = calculator.Sum;
+        }
+
+        private void Multiply_Click(object sender, RoutedEventArgs e)
+        {
+            Parse();
+            calculator.Multiply(num1, num2);
+            label1.Content = calculator.Sum;
+        }
+
+        private void Divide_Click(object sender, RoutedEventArgs e)
+        {
+            Parse();
+            calculator.Divide(num1, num2);
+            label1.Content = calculator.Sum;
+        }
+    }
+}
