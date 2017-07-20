648549703fdd48dc3b5229398504be9603d2906b
Owen Carter <Owen@kingfisher-estate.co.uk>
2017-07-20 13:31:18 +0100

    Update Calc.cs

--- Calc.cs
+++ Calc.cs
@@ -10,24 +10,29 @@ namespace Calculator.cs
     {
         private double _sum;
 
+        // class constructor intializing sum variable.
         public double Sum
         {
             get { return _sum; }
             private set { _sum = value; }
         }
 
+        // Function to add two values together.
         public void Add(double num1, double num2)
         {
             _sum = num1 + num2;
         }
+        // Function to subtract two values from each other.
         public void Minus(double num1, double num2)
         {
             _sum = num1 - num2;
         }
+        // Function to multiply two values together.
         public void Multiply(double num1, double num2)
         {
             _sum = num1 * num2;
         }
+        // Function to divide two values from each other.
         public void Divide(double num1, double num2)
         {
             _sum = num1 / num2;
